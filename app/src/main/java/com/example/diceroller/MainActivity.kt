f84cda04b3831c1a4d7ff7aaad98bdfab66b5c11
Suleiman Ibrahim <36508732+sman004@users.noreply.github.com>
2023-01-30 11:39:53 +0000

    Added image and space

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,9 +3,8 @@ package com.example.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -13,8 +12,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.textInputServiceFactory
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.birthdaywishes.R
 import com.example.diceroller.ui.theme.BirthdayWishesTheme
 
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DiceRollerTheme {
+            BirthdayWishesTheme {
                 DiceRollerApp()
             }
         }
@@ -32,8 +34,9 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun DiceRollerApp() {
         DiceWithButtonAndImage(
-            modifier = Modifier
-                .fillMaxSize().wrapContentSize(Alignment.Center)
+            Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.Center)
         )
     }
 
@@ -44,6 +47,12 @@ class MainActivity : ComponentActivity() {
             modifier = modifier,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+
+            Image(
+                painter = painterResource(R.drawable.dice_1),
+                contentDescription = "1"
+            )
+            Spacer(modifier = Modifier.height(16.dp))
             Button(onClick = { /*TODO*/ }) {
                 Text(stringResource(R.string.roll))
             }
